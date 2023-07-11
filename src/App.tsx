29bd1eb6cd620c04d9284bb216d818e1a08030c4
Ganesan K <ganesank@live.com>
2023-07-11 23:14:25 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
 import React from "react";
-import { Routes, Route, Link } from "react-router-dom";
-import Container from "react-bootstrap/Container";
-import Navbar from "react-bootstrap/Navbar";
+import { Routes, Route, Link } from "react-router-dom"; 
 import { HomePage } from "./pages/Homepage";
 import { AboutPage } from "./pages/Aboutpage";
 import { BlogPage } from "./pages/Blogpage";
@@ -10,22 +8,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 function App() {
   return (
     <div className="wrapper">
-      <Navbar className="">
-        <Container>
-          <Navbar.Brand href="/"> 
-            <h4>Ganesan Karuppaiya</h4>
-            <p>Full Stack Lead Developer</p>
-          </Navbar.Brand>
-          <Navbar.Toggle />
-          <Navbar.Collapse className="justify-content-end">
-            <Navbar.Text>
-              <Link to="/">Home</Link>
-              <Link to="/posts">Blog</Link>
-              <Link to="/about">About</Link>
-            </Navbar.Text>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
+      
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/about" element={<AboutPage />} />
