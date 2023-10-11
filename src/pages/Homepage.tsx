b42f2deff207a023c37ddbf62a1887361a21af10
Ganesan K <ganesank@live.com>
2023-10-11 20:48:04 +0530

    m

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -2,12 +2,21 @@
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
-import Stack from "react-bootstrap/Stack";
-import Accordion from "react-bootstrap/Accordion";
-import { Link } from "react-router-dom";
-import { LiaFacebook ,LiaGlobeAmericasSolid, LiaInstagram, LiaLinkedin , LiaSnapchatGhost, LiaWhatsapp ,LiaTwitter, LiaTelegramPlane, LiaGithub, LiaSkype} from "react-icons/lia";
+import {
+  LiaFacebook,
+  LiaGlobeAmericasSolid,
+  LiaInstagram,
+  LiaLinkedin,
+  LiaSnapchatGhost,
+  LiaWhatsapp,
+  LiaTwitter,
+  LiaTelegramPlane,
+  LiaGithub,
+  LiaSkype,
+  LiaEnvelopeSolid,
+  LiaPhoneSolid
+} from "react-icons/lia";
 export const HomePage = () => {
   const numbers = [
     {
@@ -65,7 +74,7 @@ export const HomePage = () => {
     <Col>
       <a href={item.url} target="_blank" title={item.title} rel="noreferrer">
         <i className={item.icon}></i>
-         {(() => {
+        {(() => {
           switch (item.title) {
             case "Website":
               return <LiaGlobeAmericasSolid />;
@@ -126,60 +135,17 @@ export const HomePage = () => {
                       </p>
                     </Card.Text>
                     <a className="primary " href="mailto:ganesank@live.com">
-                      <i className="las la-envelope"></i> Mail
+                      <LiaEnvelopeSolid /> Mail
                     </a>{" "}
                     &nbsp;
                     <a className="primary ghost" href="tel:+919943732416">
-                      <i className="las la-phone"></i> Call
+                      <LiaPhoneSolid /> Call
                     </a>
                     <div className="homeAcc">
-                    <div className="social">
-                              <Row sm={5}> {listItems}</Row>{" "}
-                            </div>
-                    {/**
-                     *   <Accordion defaultActiveKey={["0"]} alwaysOpen flush>
-                        <Accordion.Item eventKey="0">
-                          <Accordion.Header>
-                            {" "}
-                            <i className="las la-globe"></i> Social{" "}
-                          </Accordion.Header>
-                          <Accordion.Body>
-                          
-                          </Accordion.Body>
-                        </Accordion.Item>
-                        <Accordion.Item eventKey="1">
-                          <Accordion.Header>
-                            {" "}
-                            <i className="las la-file-pdf"></i> Resume{" "}
-                          </Accordion.Header>
-                          <Accordion.Body>
-                            <Row sm={3} className="social">
-                              <Col>
-                                <a
-                                  href="cv/GanesanKaruppaiyaCv.pdf"
-                                  target="_blank"
-                                >
-                                  <i className="las la-file-pdf"></i>
-                                </a>
-                              </Col>{" "}
-                              <Col>
-                                <a
-                                  href="cv/GanesanKaruppaiyaCv.docx"
-                                  target="_blank"
-                                >
-                                  <i className="las la-file-word"></i>
-                                </a>
-                              </Col>
-                              <Col>
-                                <Link to="/resume">
-                                  <i className="las la-file-code"></i>
-                                </Link>
-                              </Col>
-                            </Row>
-                          </Accordion.Body>
-                        </Accordion.Item>
-                      </Accordion>
-                     */}
+                      <div className="social">
+                        <Row sm={5}> {listItems}</Row>{" "}
+                      </div>
+                     
                     </div>
                   </Card.Body>
                 </Card>
